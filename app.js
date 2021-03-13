71f39636b62f98a0d610efe1dd1f6a91b19ad121
Faizan Hamid <faizanhamid7860@gmail.com>
2021-03-13 06:25:06 +0530

    ADDED SOME ANIMATIONS

---
+++ app.js
@@ -0,0 +1,6 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(".hero-package", { opacity : "0", rotateY : "270deg" }, { opacity : "initial", rotateY : "0deg", duration: 1}, "-=1");
